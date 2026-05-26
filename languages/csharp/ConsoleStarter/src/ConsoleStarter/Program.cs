c0b932bea829c57a8f6670b6a2c27cf2f32ca5fd
Kotrecon <ermakov_k@mail.ru>
2026-05-26 17:27:07 +0300

    feat(module2): implement basic DI registration and lifetime test

--- languages/csharp/ConsoleStarter/src/ConsoleStarter/Program.cs
+++ languages/csharp/ConsoleStarter/src/ConsoleStarter/Program.cs
@@ -1,5 +1,17 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var host = Host.CreateDefaultBuilder(args).Build();
+
+
+// Регистрация сервисов
+builder.Services.AddSingleton<ILoggerService, ConsoleLogger>();
+builder.Services.AddTransient<IDataProcessor, DataProcessor>();
+
+// Пример для Singleton/Transient
+var logger = host.Services.GetRequiredService<ILoggerService>();
+// Пример для Scoped — только через скоуп!
+using var scope = host.Services.GetRequiredService<IServiceScopeFactory>().CreateScope();
+var processor = scope.ServiceProvider.GetRequiredService<IDataProcessor>();
 
 await host.RunAsync();
